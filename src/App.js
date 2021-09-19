77bc47ed30fbc99dd5877eb5d9038d9737dbb1b3
vidhya <vidhu82@gmail.com>
2021-09-19 14:54:28 +0530

    trasnform state to game history

--- src/App.js
+++ src/App.js
@@ -3,41 +3,60 @@ import Board from './components/Board';
 import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
+//Array of objects
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([
+    {
+      board: Array(9).fill(null),
+      isXNext: true,
+    },
+  ]);
+
+  const [currentMove, setCurrentMove] = useState(0);
+  //Shortcut to get current gane state
+  const current = history[currentMove];
+
   //To switch between X and O
   const [isXNext, setIsXNext] = useState(false);
 
+  console.log('History', history);
+
   //To calculate the winner
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(current.board);
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${isXNext ? 'X' : 'O'}`;
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
     //Preventing the user to click the square again after the board is set
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      //To get the current board state
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
         //Checking whether the currently iterated position
         //is the one that is clicked by the player,if yes placing X
         if (pos === position) {
-          return isXNext ? 'X' : 'O';
+          return last.isXNext ? 'X' : 'O';
         } else return square;
       });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
+
     //Switching between values based on prev state
-    setIsXNext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
   };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
